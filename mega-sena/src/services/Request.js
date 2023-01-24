ecd6e3eb332041b0234d0ce5429a1c2dde202ba1
Max-Ranholi <ranholimax@gmail.com>
2023-01-23 22:11:31 -0300

    tentativa de status

--- mega-sena/src/services/Request.js
+++ mega-sena/src/services/Request.js
@@ -1,16 +1,15 @@
 import axios from "axios";
 
-export const selectGame = (setData, setNumbers, setName, setGameNumber) => {
+export const selectGame = (setData, setNumbers, setName, setGameNumber, prize) => {
   axios
     .get(
-      `https://apiloterias.com.br/app/resultado?loteria=quina&token=G3VxuMWAVANPw3W`
+      `https://apiloterias.com.br/app/resultado?loteria=${prize}&token=G3VxuMWAVANPw3W`
     )
     .then((res) => {
       setData(res.data);
       setNumbers(res.data.dezenas);
       setName(res.data.nome);
       setGameNumber(res.data.numero_concurso);
-      console.log(res.data);
     })
     .catch((error) => {
       console.log(error.message);
